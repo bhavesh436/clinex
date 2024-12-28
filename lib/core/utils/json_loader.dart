8c6991e9d2f49445d71ce66427928077807d1365
Bhavesh <bhaveshlalvani000@gmail.com>
2024-12-28 17:04:52 +0530

    Added json loader File for reusability

--- lib/core/utils/json_loader.dart
+++ lib/core/utils/json_loader.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
-
+// Not Used
 class JsonLoader {
   static Future<List<dynamic>> loadJson(String path) async {
     final jsonString = await rootBundle.loadString(path);
